49cdf7dbcddaff115df0b4d989238ce97e15f374
BlessedRap <576406@student.belgiumcampus.ac.za>
2021-02-24 10:26:13 +0200

    Second comment

--- Prg381.java
+++ Prg381.java
@@ -2,5 +2,6 @@ public class Prg381 {
     public static void main(String[] args) {
         System.out.println("First Java file on Git");
         //Comment
+        //Here is another comment
     }
 }
